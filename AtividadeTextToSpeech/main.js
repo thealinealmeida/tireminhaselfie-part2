03ca68948b1a4354e0b39bfbab91a08d4f0fe545
thealinealmeida <68761501+thealinealmeida@users.noreply.github.com>
2023-08-15 19:08:51 -0300

    Update main.js

--- AtividadeTextToSpeech/main.js
+++ AtividadeTextToSpeech/main.js
@@ -1,8 +1,1 @@
-const txtFala = document.getElementById("txtOutput");
 
-function textToSpeech() {
-    const synth = speechSynthesis;
-    const dadosVoz = txtFala.value;
-    const utterThis = new SpeechSynthesisUtterance(dadosVoz);
-    synth.speak(utterThis);
-}
